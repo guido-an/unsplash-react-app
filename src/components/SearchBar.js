3a6ff86bba89c7ef0b83dcb40d88b94f5bda3425
Guido Carucci <carucciguido@gmail.com>
2020-03-23 17:22:58 +0100

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,3 @@
-// import React from 'react'
-
-// class SearchBar extends React.Component {
-
-//     state = {
-//         term: ''
-//     }
-    
-//     onFormSubmit = e => {
-//         e.preventDefault()
-//         this.props.onSubmit(this.state.term)
-//     }
-//   render () {
-//     return (
-//       <div className='ui segment'>
-//         <form onSubmit={this.onFormSubmit} className='ui form'>
-//           <p>Search image</p>
-//           <input type='text' value={this.props.term} onChange={(e) => this.setState({ term: e.target.value })} />
-//         </form>
-//       </div>
-//     )
-//   }
-// }
-
-// export default SearchBar
-
 import React from 'react'
 
 class SearchBar extends React.Component {
